4a34f37a53c7c57d443048d2a4c11da51c15bed0
Vitaly Gashock <gshockv@gmail.com>
2018-10-17 13:39:44 +0300

    Initial menu screen, Motion Layout Scene 01

---
+++ app/src/main/java/com/example/motionlayouttest/MenuActivity.kt
@@ -0,0 +1,24 @@
+package com.example.motionlayouttest
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+
+    fun openFirstScene(v: View) {
+        startActivity<Scene01Activity>()
+    }
+}
+
+inline fun <reified T : Activity> Context.startActivity() {
+    val intent = Intent(this, T::class.java)
+    startActivity(intent)
+}
